c7b605b133db6879634cfe05061fd96f035bf338
Anantha27 <roboticsmind27@gmail.com>
2016-11-05 05:48:55 +0530

    connect files

---
+++ connections/connect.php
@@ -0,0 +1,12 @@
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "shivaji";
+// Create connection
+$con = new mysqli($servername, $username, $password,$dbname);
+
+
+
+?>
